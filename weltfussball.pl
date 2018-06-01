3d64f615df8df76abb4504f7bf0535c21dc58b99
fussballlinguist <39825770+fussballlinguist@users.noreply.github.com>
2018-06-01 11:09:11 +0200

    Add files via upload

---
+++ weltfussball.pl
@@ -0,0 +1,112 @@
+#! /usr/bin/perl -w
+
+###############################################################################
+# A script to crawl live texts from weltfussball.de as nice and handy xml-files
+###############################################################################
+
+use strict;
+use warnings;
+
+my $url;
+my @urls;
+my $title;
+my $date;
+my $kickoff;
+my $result;
+my $time;
+my $ticker;
+my $team1;
+my $team2;
+my $filename;
+
+my $start_url = "http://www.weltfussball.de/alle_spiele/bundesliga-2017-2018/";
+# --> Define the start page (to find under Region -> Spielplan 
+
+if ($start_url =~ /alle_spiele\/(.+?)\//) {
+	$filename = $1;
+}
+my $path = "/define/path/$filename.xml";
+# --> Define path
+
+
+
+############################
+# no changes below this line
+############################
+
+unlink($path);
+my $start_html = qx(curl -s '$start_url');
+my @lines = split /\n/, $start_html;
+foreach my $line (@lines) {
+	if ($line =~ m/(\/spielbericht\/.+?)"/) {
+		$url = "http://www.weltfussball.de" . $1 . "liveticker/";
+		push @urls, $url;
+	}
+}
+my $length = scalar @urls;
+my $counter = 0;
+open OUT, ">> $path" or die $!;
+print OUT "<corpus>\n";
+foreach my $url_game (@urls) {
+	my $html = qx(curl -s '$url_game');
+	$counter++;
+	print "Lade Nr. $counter von $length\n";
+	my @lines = split /\n/, $html;
+	foreach my $line (@lines) {
+		if ($line =~ /<title>(.+?)<\/title>/) {
+			$title = $1;
+		}
+	}
+	my @headers = split /table class="standard_tabelle" cellpadding="3" cellspacing="1"/, $html;
+	foreach my $header (@headers) {
+		if ($header =~ /<th align="center" width="35%">[\w\W]+?<th align="center">\s+(.+?)<br\/>(.+?) Uhr\s+<\/th>/) {
+			$date = $1;
+			$kickoff = $2;
+		}
+		if ($header =~ /<div class="resultat">\s+(.+?) \t+<\/div>/) {
+			$result = $1;
+		}
+		if ($header =~ /<tr>\s+<th align="center" width="35%">[\w\W]+?<a href=".+?" title="(.+?)"/) {
+			$team1 = $1;
+		}
+		if ($header =~ /<\/th>\s+<th align="center" width="35%">[\w\W]+?<a href=".+?" title=".+?">(.+?)<\/a>\s+<\/th>/) {
+			$team2 = $1;
+		}
+	}
+	print OUT "<text>
+	<url>$url_game</url>
+	<title>$title</title>
+	<team1>$team1</team1>
+	<team2>$team2</team2>
+	<date>$date</date>
+	<kickoff>$kickoff</kickoff>
+	<result>$result</result>\n";
+	my @posts = split /gross" width="50" align="center"/, $html;
+	foreach my $post (@posts) {
+		if ($post =~ /^>[\w\W]+?(\d+')/) {
+			$time = $1;
+		}
+		if ($post =~ /<div class="wfb-tickertext">([\w\W]+?)<\/div>/g) {
+			$ticker = $1;
+			$ticker =~ s/<br>/ /g; 
+			$ticker =~ s/<br.+?>/ /g; 
+			$ticker =~ s/<.+?>//g;
+			$ticker =~ s/\n//g;
+			$ticker =~ s/&/&amp;/g;
+			$ticker =~ s/>/&gt;/g;
+			$ticker =~ s/</&lt;/g;
+		}
+		if (defined $time) {
+			$time = "<time>$time</time>";
+		} else {
+			$time = "<time/>";
+		}
+		print OUT "\t$time\n\t<ticker>$ticker</ticker>\n" if defined ($ticker);
+		undef $time;
+		undef $ticker;
+	}
+	print OUT "</text>\n";
+#	sleep rand(2);
+}
+print OUT "</corpus>\n";
+close OUT
